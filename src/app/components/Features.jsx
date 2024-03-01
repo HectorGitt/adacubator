46fff033d042c372f06eb27547d5a06eff35b2f1
Hector <adeniyi.olaitanhector@yahoo.com>
2024-03-01 17:34:22 +0100

    changed card text

--- src/app/components/Features.jsx
+++ src/app/components/Features.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import Card from './Card'
 import styled from 'styled-components'
+import support from '../../../public/support_icon.png'
+import growth from '../../../public/growth_icon.png'
+import connection from '../../../public/connection_icon.png'
+import { device } from '../lib/breakpoint'
+import line from '../../../public/lines.svg'
+import Image from 'next/image'
 
 const Features = () => {
   return (
     <FeaturesCont>
-        <h2>Why <span className='waivy'>Adacubator</span></h2>
+        <h1>Why <span className='waivy'>Adacubator</span> ?</h1>
         <div>
-            <Card title='Feature 1' text='We provide the financial backing needed to take your project from concept to reality, helping you overcome any financial barriers to entry'/>
-            <Card title='Feature 2' text='We provide the financial backing needed to take your project from concept to reality, helping you overcome any financial barriers to entry'/>
-            <Card title='Feature 3' text='We provide the financial backing needed to take your project from concept to reality, helping you overcome any financial barriers to entry'/>
+            <Card title='Capital Support' icon={support} text='We provide the financial backing needed to take your project from concept to reality, helping you overcome any financial barriers to entry'/>
+            <Card title='Ecosystem Growth' icon={growth} text='By focusing exclusively on the Cardano blockchain, we aim to cultivate a thriving ecosystem of innovative solutions and applications.'/>
+            <Card title='Community Connection' icon={connection} text='Join a network of like-minded developers, entrepreneurs, and enthusiasts all working towards the common goal of advancing the Cardano ecosystem.'/>
         </div>
+        <Image className="line" src={line} />
+
     </FeaturesCont>
   )
 }
@@ -18,9 +26,32 @@ const Features = () => {
 export default Features
 
 const FeaturesCont = styled.div`
+    position: relative;
     text-align: center;
+    padding: 2rem 0;
+    min-height: 80vh;
+    padding: 0 5%;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    .line {
+      position: absolute;
+      top: 0;
+      width: 100%;
+      right: 0;
+    }
+    h2 {
+      font-size: 2rem;
+    }
     & > div {
         display: flex;
         justify-content: space-around;
+        gap: 2rem;
+    }
+
+    @media screen and (${device.md}) {
+      & > div {
+        flex-direction: column;
+    }
     }
 `
