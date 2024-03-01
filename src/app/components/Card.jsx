00fa1497de4cc560079d9a8dfb2c2e2b5c691ff3
Hector <adeniyi.olaitanhector@yahoo.com>
2024-03-01 07:30:08 +0100

    added card layout

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -13,5 +13,19 @@ const Card = ({title, text}) => {
 export default Card
 
 const CardCont = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border: 1px solid;
+    padding: 2rem;
+    max-width: 300px;
     
+    border-image: linear-gradient(107.64deg, #0098DA -4.02%, #91D8F7 82.79%);
+    background: transparent;
+    h3 {
+        font-size: 1.5rem;
+    }
+    p {
+        color: #c0c0c0cd;
+    }
 `
