641645ed59afc3a80d3b5cb3625ad066426a2ec9
Hector <adeniyi.olaitanhector@yahoo.com>
2024-03-01 07:28:52 +0100

    added nav

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -1,23 +1,127 @@
-import Image from "next/image"
-import styled from "styled-components"
-import logo from ''
+import React, {useState} from 'react'
+import { CgMenu } from 'react-icons/cg'
+import {IoCloseCircleOutline} from 'react-icons/io5';
+import logo from '../../../public/logo.png'
+import styled from 'styled-components'
+import { device } from '@/app/lib/breakpoint'
+import Image from 'next/image';
+import Button from './Button';
 
 const Nav = () => {
+    const [nav, setNav] = useState(false);
+  const toggleNav = () => {
+    setNav(!nav);
+  };
+  const closeNav = () => {
+    setNav(false);
+  };
   return (
-    <Nav>
-        <Image src='logo.png' />
-        <ul>
-            <li>About</li>
-            <li>Mission</li>
-            <li>Pitch To Us</li>
+    <NavBar>
+        <a href='/' ><Image alt='adacubator logo' src={logo}/></a>
+        <ul className='navlist-pc'>
+            <li><a href='#about'>About</a></li>
+            <li><a href='#misson'>Misson</a></li>
+            <li><a href='#pitch'>Pitch To Us</a></li>
         </ul>
-        <button>Contact Us</button>
-    </Nav>
+        {/* <MainButton target className='modal-button' to='/buy' text='Buy CGI' /> */}
+        <Button text='Contact Us' target to='/buy' />
+        
+        <CgMenu onClick={toggleNav} className='menu-button' id='menu' size='4rem' />
+        
+
+        <div className={nav? 'navlist navlist-mobile': 'navlist'}>
+            <div className='top-mobile'>
+                <Image alt='CardanoGPT logo' src={logo}/>
+                <IoCloseCircleOutline onClick={closeNav} 
+                className='menu-button-mobile' id='menu' size='4rem' />
+            </div>
+            <ul>
+                <li onClick={closeNav}><a href='#About'>About</a></li>
+                <li onClick={closeNav}><a href='#misson'>Misson</a></li>
+                <li onClick={closeNav}><a href='#pitch'>Pitch To Us</a></li>
+                <li onClick={closeNav}></li>
+            </ul>
+            {/* <Popup trigger={<button className='modal-button'>Buy CGI</button>} position="right center" modal>
+            <div className='popup'>Coming Soon!!</div>
+            </Popup> */}
+            {/* <MainButton target to='/buy' text='Buy CGI' /> */}
+        </div>
+
+    </NavBar>
   )
 }
 
 export default Nav
 
-const Navbar = styled.nav`
+const NavBar = styled.nav`
+    .menu-button {
+        display: none;
+    }
     
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem 1%;
+    border-bottom: 1px solid #5d6a81;
+    ul {
+        display: flex;
+        justify-content: space-between;
+        list-style: none;
+        color: white;
+        min-width: 35%;
+        gap: 0.5rem;
+    }
+    .navlist-mobile {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100vh;
+        background: #19202B;
+        display: flex;
+        flex-direction: column;
+        z-index: 100;
+        padding: 2%;
+        ul{
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            text-align: start;
+            margin: 2rem 0;
+        }
+        li {
+            margin: 1rem;
+            font-size: 1.5rem;
+            cursor: pointer;
+        }
+        .action-button {
+            display: inline-block;
+            width: 70%;
+            text-align: center;
+        }
+    }
+    .navlist {
+        display: none;
+    }
+    .top-mobile {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
+    }
+    @media screen and (${device.md}) {
+        .navlist-pc, button {
+            display: none;
+        }
+        .menu-button {
+            display: flex;
+
+        }
+        .navlist-mobile {
+            display: block;
+        }
+        &> .action-button {
+            display: none;
+        }
+    }
 `
