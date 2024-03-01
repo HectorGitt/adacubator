8a2173d9852d905f201bb37337ef1f08ec2ae02f
Hector <adeniyi.olaitanhector@yahoo.com>
2024-03-01 07:27:23 +0100

    added fonts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,9 @@
-import { Inter } from "next/font/google";
+import {Inter,  Open_Sans } from "next/font/google";
 import "./globals.css";
 import StyledComponentsRegistry from './lib/registry'
 
 const inter = Inter({ subsets: ["latin"] });
+const openSans = Open_Sans({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Adacubator",
@@ -12,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={openSans.className}>
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>  
       </body>
     </html>
