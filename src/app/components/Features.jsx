543cb112f18bc8c4abde401b59318a86e2dcce3c
Hector <adeniyi.olaitanhector@yahoo.com>
2024-03-01 07:28:38 +0100

    added features layout

---
+++ src/app/components/Features.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Card from './Card'
+import styled from 'styled-components'
+
+const Features = () => {
+  return (
+    <FeaturesCont>
+        <h2>Why Adacubator</h2>
+        <div>
+            <Card title='Feature 1' text='We provide the financial backing needed to take your project from concept to reality, helping you overcome any financial barriers to entry'/>
+            <Card title='Feature 2' text='We provide the financial backing needed to take your project from concept to reality, helping you overcome any financial barriers to entry'/>
+            <Card title='Feature 3' text='We provide the financial backing needed to take your project from concept to reality, helping you overcome any financial barriers to entry'/>
+        </div>
+    </FeaturesCont>
+  )
+}
+
+export default Features
+
+const FeaturesCont = styled.div`
+    text-align: center;
+    & > div {
+        display: flex;
+        justify-content: space-around;
+    }
+`
