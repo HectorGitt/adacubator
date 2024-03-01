eab43643ae150c9446c2915ed822a8cdb915c7bd
Hector <adeniyi.olaitanhector@yahoo.com>
2024-03-01 07:27:53 +0100

    added component layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,19 @@
 "use client";
 import Image from "next/image";
 import styled from "styled-components";
+import Nav from "./components/Nav";
+import HeroSection from "./components/HeroSection";
+import Features from "./components/Features";
 
 export default function Home() {
   return (
-    <main>
-      
-    </main>
+    <Main>
+      <Nav/>
+      <HeroSection />
+      <Features/>
+    </Main>
   );
 }
+const Main = styled.main`
 
-const Flex = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-`;
+`
