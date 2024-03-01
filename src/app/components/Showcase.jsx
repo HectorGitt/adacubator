349316a585465ba8db0819d4713ccac4a52afcd5
Hector <adeniyi.olaitanhector@yahoo.com>
2024-03-01 15:25:49 +0100

    Update Showcase component with Cardano branding

--- src/app/components/Showcase.jsx
+++ src/app/components/Showcase.jsx
@@ -4,7 +4,7 @@ const Showcase = () => {
   return (
     <ShowcaseCont>
         <h2>
-            Let&quot;s Accelerate Your Success on Cardano 
+            Let&ldquo;s <span className='waivy'>Accelerate</span> Your Success <br/>on <span className='waivy'>Cardano</span>
         </h2>
         <p>For inquiries, partnership opportunities, or to submit your project for consideration<br/>please contact us at <a href='mailto:info@aducubator.com'>info@aducubator.com</a></p>
     </ShowcaseCont>
@@ -14,5 +14,17 @@ const Showcase = () => {
 export default Showcase
 
 const ShowcaseCont = styled.div`
+  text-align: center;
+  min-height: 50vh;
+  display: flex;
+  flex-direction: column;
+  h2 {
+    font-size: 2rem;
+    font-weight: 700;
+    margin: 2rem 0;
+  }
+  a {
+    color: #0098DA;
+  }
 
 `
