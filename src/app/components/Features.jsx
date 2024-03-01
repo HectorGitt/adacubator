41646d589326b7b6e1cc412790852d2ff71c311c
Hector <adeniyi.olaitanhector@yahoo.com>
2024-03-01 15:24:15 +0100

    Update heading in Features component

--- src/app/components/Features.jsx
+++ src/app/components/Features.jsx
@@ -5,7 +5,7 @@ import styled from 'styled-components'
 const Features = () => {
   return (
     <FeaturesCont>
-        <h2>Why Adacubator</h2>
+        <h2>Why <span className='waivy'>Adacubator</span></h2>
         <div>
             <Card title='Feature 1' text='We provide the financial backing needed to take your project from concept to reality, helping you overcome any financial barriers to entry'/>
             <Card title='Feature 2' text='We provide the financial backing needed to take your project from concept to reality, helping you overcome any financial barriers to entry'/>
