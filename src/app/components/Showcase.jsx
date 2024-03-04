2b5df9404d15e32ff411ffcfa0a4a2b8336aec84
Hector <adeniyi.olaitanhector@yahoo.com>
2024-03-05 00:15:41 +0100

    Update Showcase component styling

--- src/app/components/Showcase.jsx
+++ src/app/components/Showcase.jsx
@@ -3,9 +3,9 @@ import styled from 'styled-components'
 const Showcase = () => {
   return (
     <ShowcaseCont>
-      <h2 data-aos='zoom-in'>
-            Let&ldquo;s <span className='waivy'>Accelerate</span> Your Success <br/>on <span className='waivy'>Cardano</span>
-        </h2>
+      <h1 data-aos='zoom-in'>
+            Let&lsquo;s <span className='waivy'>Accelerate</span> Your Success <br/>on <span className='waivy'>Cardano</span>
+        </h1>
         <p data-aos='fade-up'>For inquiries, partnership opportunities, or to submit your project for consideration<br/>please contact us at <a className='waivy' href='mailto:info@aducubator.com'>info@aducubator.com</a></p>
         <div className='circle'></div>
     </ShowcaseCont>
@@ -17,13 +17,13 @@ export default Showcase
 const ShowcaseCont = styled.div`
   position: relative;
   text-align: center;
-  min-height: 400px;
+  min-height: 150px;
   margin: 8rem 0;
   display: flex;
   flex-direction: column;
   justify-content: center;
-  h2 {
-    font-size: 2rem;
+  h1 {
+    font-size: 2.5rem;
     font-weight: 700;
     margin: 2rem 0;
   }
