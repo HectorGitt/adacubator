1161004deca0e82dbfc7d0db8eb38a687cfe00b4
Hector <adeniyi.olaitanhector@yahoo.com>
2024-06-12 12:45:20 +0100

    fix button

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -2,22 +2,23 @@ import React from "react";
 import styled from "styled-components";
 
 const Button = ({ className, text, dataAos, to }) => {
-  return (
-    <ButtonCont className={className} data-aos={dataAos} href={to}>
-      {text}
-    </ButtonCont>
-  );
+	return (
+		<ButtonCont className={className} data-aos={dataAos} href={to}>
+			{text}
+		</ButtonCont>
+	);
 };
 
 export default Button;
 
 const ButtonCont = styled.a`
-  padding: 14px 28px;
-  font-size: 1.2rem;
-  background: linear-gradient(107.64deg, #0098da -4.02%, #91d8f7 82.79%);
-  border: none;
-  border-radius: 21px;
-  color: white;
-  cursor: pointer;
-  width: fit-content;
+	padding: 14px 28px;
+	font-size: 1.2rem;
+	background: linear-gradient(107.64deg, #0098da -4.02%, #91d8f7 82.79%);
+	border: none;
+	border-radius: 21px;
+	color: white;
+	cursor: pointer;
+	width: fit-content;
+	z-index: 10;
 `;
